bb5c8d57dc99e112604013bfbc320dbb54d75fda
Shubhashis Roy <shubhashisroy360@gmail.com>
2026-04-05 17:38:44 +0530

    fix: remove html from chatbot

--- app/routes/chatbot.tsx
+++ app/routes/chatbot.tsx
@@ -14,23 +14,21 @@ export default function Chatbot() {
   const [params] = useSearchParams();
   const shop = params.get("shop");
   return (
-    <html>
-      <body>
-        <h3
-          style={{
-            textAlign: "center",
-            fontWeight: "700",
-            fontSize: "18px",
-            margin: "12px 0",
-          }}
-        >
-          AI Shoe Assistant
-        </h3>
-        <div id="chatbot-root">
-          {/* <ChatBotCore /> */}
-          <ChatBotCore shop={shop} />
-        </div>
-      </body>
-    </html>
+    <div>
+      <h3
+        style={{
+          textAlign: "center",
+          fontWeight: "700",
+          fontSize: "18px",
+          margin: "12px 0",
+        }}
+      >
+        AI Shoe Assistant
+      </h3>
+      <div id="chatbot-root">
+        {/* <ChatBotCore /> */}
+        <ChatBotCore shop={shop} />
+      </div>
+    </div>
   );
 }
